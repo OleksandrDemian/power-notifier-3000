406fe2eb4ec80c632844290f0bb695fd4e52e522
Oleksandr Demian <oleksandrovsky@gmail.com>
2020-05-31 11:13:30 +0200

    Mobile bug fix

--- src/elements/container.js
+++ src/elements/container.js
@@ -1,6 +1,6 @@
 const utils = require("../utils");
 
-const DEFAULT_CSS = {
+const DEFAULT_CSS = () => ({
 	position: "fixed",
 	right: "0px",
 	bottom: "0px",
@@ -8,13 +8,24 @@ const DEFAULT_CSS = {
 	display: "flex",
 	flexDirection: "column-reverse",
 	overflowY: "hidden"
+});
+
+const CSS = () => {
+	const css = DEFAULT_CSS();
+	
+	if(window.matchMedia("(max-width: 768px)").matches){
+		css.width = "100%";
+		css.margin = "5px";
+	}
+	
+	return css;
 };
 
 function createContainer(css = null) {
 	const div = document.createElement("div");
 	div.id = "power-notifier-3000-container";
 	
-	utils.applyCss(div, DEFAULT_CSS);
+	utils.applyCss(div, CSS());
 	if(css != null){
 		utils.applyCss(div, css);
 	}
